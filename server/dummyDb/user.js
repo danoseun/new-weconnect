856205099d78952e6d26b1b47d29da7fd0c080f4
danoseun <ayodejisomefun@yahoo.com>
2018-08-10 14:02:44 +0100

    create in-memory-data for user object

---
+++ server/dummyDb/user.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    id: 1,
+    firstName: 'Pelenge',
+    lastName: 'John',
+    email: 'pelengejohn@gmail.com',
+    username: 'johnpelenge',
+    password: 'jpelenge'
+  },
+
+  {
+    id: 2,
+    firstName: 'Jason',
+    lastName: 'Bourne',
+    email: 'jasonbourne@gmail.com',
+    username: 'jasonbourne',
+    password: 'jbourne'
+  }
+];
+
+export default users;
