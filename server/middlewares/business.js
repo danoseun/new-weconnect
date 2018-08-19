12ec42ce5aa620ea1a109fd4ea69ab4b94b8a881
danoseun <ayodejisomefun@yahoo.com>
2018-08-19 03:25:42 +0100

    Use appropriate status code

--- server/middlewares/business.js
+++ server/middlewares/business.js
@@ -42,26 +42,26 @@ class BusinessValidator {
 
     // Business name
     if (businessName === undefined) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Please specify business name'
       });
     }
 
     if (businessName === '') {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Business name field can not be empty'
       });
     }
 
     businessName = businessName.toUpperCase().trim();
     if (businessName.length < 1 || businessName.lebgth > 30) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Ensure that your business name is between 1 to 30 characters'
       });
     }
     const alphaNumeric = /^[A-Za-z0-9]+$/;
     if (!alphaNumeric.test(businessName)) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Only alphanumeric characters are allowed'
       });
     }
@@ -73,46 +73,46 @@ class BusinessValidator {
     }
     // description
     if (description === undefined) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Please tell us a brief summary of your business'
       });
     }
     if (description === '') {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Description field can not be empty'
       });
     }
     description = description.toLowerCase().trim();
     if (description.length < 1 || description.length > 50) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Please restrict the description between 1 and 50 characters'
       });
     }
     if (!alphaNumeric.test(description)) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Only alphanumeric characters are allowed in this field'
       });
     }
     // Location
     if (location === undefined) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Please specify location'
       });
     }
     if (location === '') {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Location field can not be empty'
       });
     }
     location = location.toLowerCase().trim();
     const nameValidCharacters = /^[A-Za-z]+$/;
     if (!nameValidCharacters.test(location)) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'location should contain alphabets only'
       });
     }
     if (location.length < 2 || location.length > 30) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'We recommend for sanity sake that location should be between 2 and 30 characters'
       });
     }
@@ -123,29 +123,29 @@ class BusinessValidator {
       });
     }
     if (category === '') {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Category field can not be empty'
       });
     }
     category = category.toLowerCase().trim();
     if (!nameValidCharacters.test(category)) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Category can contain alphabets only'
       });
     }
     if (category.length < 2 || category.length > 15) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'For sanity sake, we recommend that characters should be between 2 and 15 characters'
       });
     }
     // Email
     if (email === undefined) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'You have made no input'
       });
     }
     if (email === '') {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Email field can not be empty'
       });
     }
@@ -153,7 +153,7 @@ class BusinessValidator {
     /* eslint-disable no-useless-escape */
     const emailVerifier = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     if (!emailVerifier.test(email)) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Your email format is invalid'
       });
     }
@@ -165,21 +165,22 @@ class BusinessValidator {
     }
     // Phone number
     if (phoneNumber === undefined) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Supply your phone number'
       });
     }
     if (phoneNumber === '') {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Phone number can not be empty'
       });
     }
     const phoneNumberChecker = /^\d{11}$/;
     if (!phoneNumberChecker.test(phoneNumber)) {
-      return res.status(406).json({
+      return res.status(400).json({
         message: 'Invalid phone number'
       });
     }
+    next();
   }
 }
 export default BusinessValidator;
