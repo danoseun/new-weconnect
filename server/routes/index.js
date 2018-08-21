16473fe1c44699d3fcae30a90b726bbb7f8668b7
danoseun <ayodejisomefun@yahoo.com>
2018-08-21 20:25:46 +0100

    Add business router to index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 // This file consolidates all the imports in this directory
 import userRouter from './user';
+import businessRouter from './business';
 import defaultRouter from './default';
 
 export default {
   userRouter,
+  businessRouter,
   defaultRouter
 };
