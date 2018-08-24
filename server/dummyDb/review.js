a59219affbaea37f88fbf0ce17c52a2356d91d0c
danoseun <ayodejisomefun@yahoo.com>
2018-08-24 02:38:38 +0100

    Create in-memory data for review

---
+++ server/dummyDb/review.js
@@ -0,0 +1,14 @@
+const reviews = [
+  {
+    id: 1,
+    businessId: 1,
+    content: 'Best fumigators in town'
+  },
+
+  {
+    id: 2,
+    businessId: 2,
+    content: 'Patronise us without regrets',
+  },
+];
+export default reviews;
