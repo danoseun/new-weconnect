733cdceb43e08b228340f43975fa51be23c1718e
danoseun <ayodejisomefun@yahoo.com>
2018-09-18 14:14:59 +0100

    Modify database script

--- server/db/db.js
+++ server/db/db.js
@@ -1,31 +1,34 @@
 import pool from './connect';
 
-const createUserTable = `CREATE TABLE IF NOT EXISTS users(
+const createUserTable = `DROP TABLE users CASCADE; 
+            CREATE TABLE users(
             user_id SERIAL PRIMARY KEY NOT NULL,
-            first_name CHARACTER VARYING(20) NOT NULL,
-            last_name CHARACTER VARYING(20) NOT NULL,
+            firstName CHARACTER VARYING(20) NOT NULL,
+            lastName CHARACTER VARYING(20) NOT NULL,
             username CHARACTER VARYING(10) UNIQUE NOT NULL,
             email CHARACTER VARYING(30) UNIQUE NOT NULL,
             password CHARACTER VARYING(255) NOT NULL
         )`;
 
-const createBusinessTable = `CREATE TABLE IF NOT EXISTS businesses(
+const createBusinessTable = `DROP TABLE businesses CASCADE; 
+            CREATE TABLE businesses(
             business_id SERIAL PRIMARY KEY NOT NULL,
             owner_id INTEGER NOT NULL,
-            business_name CHARACTER VARYING(100) UNIQUE NOT NULL,
+            businessName CHARACTER VARYING(100) UNIQUE NOT NULL,
             description CHARACTER VARYING(50) NOT NULL,
             email CHARACTER VARYING(50) UNIQUE NOT NULL,
             location CHARACTER VARYING(30) NOT NULL,
             category CHARACTER VARYING(15) NOT NULL,
-            phone_number CHARACTER VARYING(20) NOT NULL,
+            phoneNumber CHARACTER VARYING(20) NOT NULL,
             FOREIGN KEY (owner_id) REFERENCES users (user_id) ON DELETE CASCADE
         )`;
 
-const createReviewTable = `CREATE TABLE IF NOT EXISTS reviews(
+const createReviewTable = `DROP TABLE reviews CASCADE; 
+            CREATE TABLE reviews(
             review_id SERIAL PRIMARY KEY NOT NULL,
             bus_id INTEGER NOT NULL,
             owner_id INTEGER NOT NULL,
-            content CHARACTER VARYING(100) NOT NULL,
+            content CHARACTER VARYING(500) NOT NULL,
             FOREIGN KEY (bus_id) REFERENCES businesses (business_id) ON DELETE CASCADE,
             FOREIGN KEY (owner_id) REFERENCES users (user_id) ON DELETE CASCADE
         )`;
@@ -48,4 +51,4 @@ pool.query(createBusinessTable)
 pool.query(createReviewTable)
   .then((result) => {
     console.log('reviewTable', result);
-  }).catch(error => console.log(`reviewTable ${error}`));
+  }).catch(error => console.log(`reviewTable ${error.message}`));
